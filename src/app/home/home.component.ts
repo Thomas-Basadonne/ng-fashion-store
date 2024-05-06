c7d85f513e6cfe89944c34fc1825daddaf5f9e88
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-05-06 15:27:56 +0200

    metodo get per prendere dati dal back-end

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private productService: ProductsService) {}
 
+  ngOnInit(): void {
+    this.productService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
 }
